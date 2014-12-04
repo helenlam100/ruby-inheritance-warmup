10d416f6e9d48b83f948e780ed90b94363395964
Helen Lam <helen.f.lam100@gmail.com>
2014-12-04 13:19:17 -0800

    initial commit

--- lib/stereotypes.rb
+++ lib/stereotypes.rb
@@ -12,30 +12,23 @@ class Person
   end
 end
 
-class Aussie
+class Oceanian < Person
+  def greeting
+    "G'day mate!"
+  end
+end
+
+
+class Aussie<Oceanian
   def type_of_person
     "an Aussie"
   end
-
-  def greeting
-    "G'day mate!"
-  end
-
-  def description
-    "When #{type_of_person} greets you, they say \"#{greeting}\""
-  end
 end
 
-class Kiwi
+class Kiwi<Oceanian
   def type_of_person
     "a Kiwi"
   end
-
-  def greeting
-    "G'day mate!"
-  end
-
-  def description
-    "When #{type_of_person} greets you, they say \"#{greeting}\""
-  end
 end
+
+#inheritance is a bad thing, and try not to inherit classes from each other. It is composition over inheritance. 
